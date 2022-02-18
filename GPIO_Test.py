7a26265ea0cfbce012029a0cf8bbcf9758e63ea5
Avnoor <54592053+avnoor-488@users.noreply.github.com>
2022-02-18 12:49:26 +0530

    Create GPIO_Test.py

---
+++ GPIO_Test.py
@@ -0,0 +1,88 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+
+##GPIO.cleanup()
+GPIO.setwarnings(False)
+
+gp2 = 2
+gp3 = 3
+gp4 = 4
+gp5 = 5
+gp6 = 6
+gp7 = 7
+gp8 = 8
+gp9 = 9
+gp10 = 10
+gp11 = 11
+gp12 = 12
+gp13 = 13
+gp14 = 14
+gp15 = 15
+gp16 = 16
+gp17 = 17
+gp18 = 18
+gp19 = 19
+gp20 = 20
+gp21 = 21
+gp22 = 22
+gp23 = 23
+gp24 = 24
+gp25 = 25
+gp26 = 26
+gp27 = 27
+
+GPIO.setup(gp2,GPIO.OUT)
+GPIO.setup(gp3,GPIO.OUT)
+GPIO.setup(gp4,GPIO.OUT)
+GPIO.setup(gp5,GPIO.OUT)
+GPIO.setup(gp6,GPIO.OUT)
+GPIO.setup(gp7,GPIO.OUT)
+GPIO.setup(gp8,GPIO.OUT)
+GPIO.setup(gp9,GPIO.OUT)
+GPIO.setup(gp10,GPIO.OUT)
+GPIO.setup(gp11,GPIO.OUT)
+GPIO.setup(gp12,GPIO.OUT)
+GPIO.setup(gp13,GPIO.OUT)
+GPIO.setup(gp14,GPIO.OUT)
+GPIO.setup(gp15,GPIO.OUT)
+GPIO.setup(gp16,GPIO.OUT)
+GPIO.setup(gp17,GPIO.OUT)
+GPIO.setup(gp18,GPIO.OUT)
+GPIO.setup(gp19,GPIO.OUT)
+GPIO.setup(gp20,GPIO.OUT)
+GPIO.setup(gp21,GPIO.OUT)
+GPIO.setup(gp22,GPIO.OUT)
+GPIO.setup(gp23,GPIO.OUT)
+GPIO.setup(gp24,GPIO.OUT)
+GPIO.setup(gp25,GPIO.OUT)
+GPIO.setup(gp26,GPIO.OUT)
+GPIO.setup(gp27,GPIO.OUT)
+##GPIO.cleanup()
+
+GPIO.output(gp2, True)
+GPIO.output(gp3, True)
+GPIO.output(gp4, True)
+GPIO.output(gp5, True)
+GPIO.output(gp6, True)
+GPIO.output(gp7, True)
+GPIO.output(gp8, True)
+GPIO.output(gp9, True)
+GPIO.output(gp10, True)
+GPIO.output(gp11, True)
+GPIO.output(gp12, True)
+GPIO.output(gp13, True)
+GPIO.output(gp14, True)
+GPIO.output(gp15, True)
+GPIO.output(gp16, True)
+GPIO.output(gp17, True)
+GPIO.output(gp18, True)
+GPIO.output(gp19, True)
+GPIO.output(gp20, True)
+GPIO.output(gp21, True)
+GPIO.output(gp22, True)
+GPIO.output(gp23, True)
+GPIO.output(gp24, True)
+GPIO.output(gp25, True)
+GPIO.output(gp26, True)
+GPIO.output(gp27, True)
